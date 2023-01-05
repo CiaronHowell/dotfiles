2b4c5fb5c4a53002d5dc07fd44da11905f59bdd4
Howell <ciaron.howell@stfc.ac.uk>
2023-01-05 20:07:06 +0000

    Nvim checkpoint

--- .config/nvim/lua/lsp-setup.lua
+++ .config/nvim/lua/lsp-setup.lua
@@ -1,20 +1,143 @@
+local util = require "lspconfig.util"
+-- LSP Settings
+-- This function gets run when an LSP connects to a particular buffer.
+local on_attach = function(_, bufnr)
+  local nmap = function(keys, func, desc)
+    if desc then
+      desc = 'LSP: ' .. desc
+    end
+
+    vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
+  end
+
+  nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+  nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+
+  nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
+  nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+  nmap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
+  nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
+  nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+  nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+
+  -- Diagnostics
+  nmap(']r', vim.diagnostic.open_float, 'Expand Diagnostic')
+  nmap(']s', vim.diagnostic.show, 'Show Diagnostics')
+
+  -- See `:help K` for why this keymap
+  nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
+  nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
+
+  -- Lesser used LSP functionality
+  nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+  nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
+  nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
+  nmap('<leader>wl', function()
+    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+  end, '[W]orkspace [L]ist Folders')
+
+  -- Create a command `:Format` local to the LSP buffer
+  vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
+    if vim.lsp.buf.format then
+      vim.lsp.buf.format()
+    elseif vim.lsp.buf.formatting then
+      vim.lsp.buf.formatting()
+    end
+  end, { desc = 'Format current buffer with LSP' })
+
+  -- AutoCMD below can be a bit janky
+  -- vim.api.nvim_create_autocmd("BufWritePre", {
+  --   pattern = {
+  --     "*"
+  --   },
+  --   command = [[lua vim.lsp.buf.formatting_sync()]]
+  -- })
+end
+
+-- Setup mason so it can manage external tooling
 require('mason').setup()
 
-local m_lspconfig = require('mason-lspconfig')
+-- Enable the following language servers
+-- Feel free to add/remove any LSPs that you want here. They will automatically be installed
+local servers = { 'clangd', 'rust_analyzer', 'pyright', 'tsserver', 'sumneko_lua', 'gopls', 'jsonls', 'dockerls' }
 
-m_lspconfig.setup({
-  ensure_installed = { "lua-language-server", "javascript", "typescript" }
-})
-m_lspconfig.setup_handlers({
-  function(server_name)
-    require("lspconfig")[server_name].setup {
-      on_attach = require("shared").on_attach,
+-- Ensure the servers above are installed
+require('mason-lspconfig').setup {
+  ensure_installed = servers,
+}
+
+-- nvim-cmp supports additional completion capabilities
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+
+for _, lsp in ipairs(servers) do
+  require('lspconfig')[lsp].setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+  }
+end
+
+-- Turn on lsp status information
+require('fidget').setup()
+
+-- Make runtime files discoverable to the server
+local runtime_path = vim.split(package.path, ';')
+table.insert(runtime_path, 'lua/?.lua')
+table.insert(runtime_path, 'lua/?/init.lua')
+
+require('lspconfig').sumneko_lua.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  settings = {
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most likely LuaJIT)
+        version = 'LuaJIT',
+        -- Setup your lua path
+        path = runtime_path,
+      },
+      diagnostics = {
+        globals = { 'vim' },
+      },
+      workspace = { library = vim.api.nvim_get_runtime_file('', true) },
+      -- Do not send telemetry data containing a randomized but unique identifier
+      telemetry = { enable = false },
+    },
+  },
+}
+
+require('lspconfig').yamlls.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  settings = {
+    yaml = {
+      schemas = { kubernetes = "*.yaml" },
     }
-  end,
-})
+  }
+}
 
-require('mason-tool-installer').setup {
-  ensure_installed = { "codelldb", "stylua", "shfmt", "shellcheck", "black", "isort", "prettierd" },
-  auto_update = false,
-  run_on_start = true,
+require('lspconfig').sourcery.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  init_options = {
+    token = 'user_RSaPnwQGqWM9Gg3qiSQogix8KT3dWUTF32JdWrp2tvXGvXl1CDnMYXFMjkU'
+  }
+}
+
+-- efm being used for lsps that don't contain linters
+require('lspconfig').efm.setup {
+  on_attach = on_attach,
+  flags = {
+    debounce_text_changes = 150,
+  },
+  init_options = { documentFormatting = true },
+  filetypes = { 'python' },
+  settings = {
+    rootMarkers = { ".git/" },
+    languages = {
+      python = {
+        { formatCommand = "black --quiet -", formatStdin = true }
+      }
+    }
+  }
 }
--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -16,15 +16,10 @@ return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
 
-  -- Theme
+  -- Themes
   use 'joshdick/onedark.vim'
-
-  -- For code highlighting
-  use {
-    'nvim-treesitter/nvim-treesitter',
-    run = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
-  }
-  use 'nvim-treesitter/nvim-treesitter-context'
+  use 'folke/tokyonight.nvim'
+  use 'tjdevries/colorbuddy.vim'
 
   use {
     'nvim-telescope/telescope.nvim',
@@ -34,21 +29,26 @@ return require('packer').startup(function(use)
     },
     config = function()
       require('telescope').load_extension('harpoon')
+      require('telescope').load_extension('git_worktree')
     end
   }
+  -- Fuzzy Finder Algorithm which requires local dependencies to be built. Only load if `make` is available
+  use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make', cond = vim.fn.executable 'make' == 1 }
+
   use 'ThePrimeagen/harpoon'
+  use 'ThePrimeagen/git-worktree.nvim'
 
   -- Avoids overhead of creating filetypes manually e.g., *.astro
   use 'nathom/filetype.nvim'
 
   -- For LSPs, Linters, and Formatters
   use {
-    "williamboman/mason.nvim",
+    "neovim/nvim-lspconfig",
     requires = {
-      "neovim/nvim-lspconfig",
-      'onsails/lspkind.nvim',
+      "williamboman/mason.nvim",
       "williamboman/mason-lspconfig.nvim",
-      'WhoIsSethDaniel/mason-tool-installer.nvim',
+      'onsails/lspkind.nvim',
+      'j-hui/fidget.nvim'
     },
     config = function()
       require('lsp-setup')
@@ -57,39 +57,71 @@ return require('packer').startup(function(use)
 
   -- For auto completion, snippets, and suggestions
   use {
-    "hrsh7th/nvim-cmp",
+    'hrsh7th/nvim-cmp',
     requires = {
-      "hrsh7th/cmp-buffer", "hrsh7th/cmp-nvim-lsp",
-      'quangnguyen30192/cmp-nvim-ultisnips', 'hrsh7th/cmp-nvim-lua',
-      'octaltree/cmp-look', 'hrsh7th/cmp-path', 'hrsh7th/cmp-calc',
-      'f3fora/cmp-spell', 'hrsh7th/cmp-emoji'
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-nvim-lsp",
+      'hrsh7th/cmp-nvim-lua',
+      'quangnguyen30192/cmp-nvim-ultisnips',
+      'octaltree/cmp-look',
+      'hrsh7th/cmp-path',
+      'hrsh7th/cmp-calc',
+      'f3fora/cmp-spell',
+      'hrsh7th/cmp-emoji',
+      'saadparwaiz1/cmp_luasnip',
+      'L3MON4D3/LuaSnip',
+      'rafamadriz/friendly-snippets'
     },
     config = function()
       require('cmp-setup')
     end
   }
+
+  -- For code highlighting
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    run = function()
+      require('nvim-treesitter.install').update({ with_sync = true })
+    end,
+    config = function ()
+      require('treesitter-configs')
+    end
+  }
+  use { -- Additional text objects via treesitter
+    'nvim-treesitter/nvim-treesitter-textobjects',
+    after = 'nvim-treesitter',
+  }
+  use {
+    'nvim-treesitter/nvim-treesitter-context',
+    after = 'nvim-treesitter',
+  }
+
   use {
     'tzachar/cmp-tabnine',
     run = './install.sh',
     requires = 'hrsh7th/nvim-cmp'
   }
-  use 'saadparwaiz1/cmp_luasnip'
-  use 'L3MON4D3/LuaSnip'
-  use 'SirVer/ultisnips'
 
-  -- For a spicy statusline
-  --use({
-  --'glepnir/galaxyline.nvim',
-  --branch = 'main',
-  -- your statusline
-  --config = function()
-  --require('my_statusline')
-  --end,
-  -- some optional icons
-  --requires = { 'kyazdani42/nvim-web-devicons', opt = true },
-  --})
+  use 'tpope/vim-surround'
+  use 'terrortylor/nvim-comment'
+  use {
+    'numToStr/Comment.nvim',
+    config = function()
+      require('Comment').setup()
+    end
+  }
+  use {
+    'folke/todo-comments.nvim',
+    requires = 'nvim-lua/plenary.nvim',
+    config = function()
+      require('todo-comments').setup {
+        -- NOTE: Defaults are satisfactory
+      }
+    end
+  }
 
   -- Useful for git blame and other cool git stuffs
+  use 'tpope/vim-fugitive'
   use {
     'lewis6991/gitsigns.nvim',
     config = function()
@@ -105,16 +137,16 @@ return require('packer').startup(function(use)
     end
   }
 
-  use { 'tjdevries/colorbuddy.vim' }
-
   use {
-    'numToStr/Comment.nvim',
-    config = function()
-      require('Comment').setup()
+    'lukas-reineke/indent-blankline.nvim',
+    config = function ()
+      require('indent_blankline').setup {
+        char = '┊',
+        show_trailing_blankline_indent = false,
+      }
     end
   }
-
-  use "lukas-reineke/indent-blankline.nvim"
+  use 'tpope/vim-sleuth'
 
   use {
     'kyazdani42/nvim-tree.lua',
@@ -124,6 +156,46 @@ return require('packer').startup(function(use)
     config = {
       require('nvim-tree').setup()
     }
+  }
+
+  use {
+    'KadoBOT/nvim-spotify',
+    config = function()
+      local spotify = require 'nvim-spotify'
+
+      spotify.setup {
+        -- default opts
+        status = {
+          update_interval = 10000, -- the interval (ms) to check for what's currently playing
+          format = '%s %t by %a' -- spotify-tui --format argument
+        }
+      }
+    end,
+    run = 'make'
+  }
+
+  use {
+    'nvim-lualine/lualine.nvim',
+    after = 'nvim-spotify',
+    requires = {
+      'kyazdani42/nvim-web-devicons',
+      opt = true
+    },
+    config = function()
+      local status = require('nvim-spotify').status
+      status:start()
+
+      require('lualine').setup {
+        options = {
+          theme = 'tokyonight'
+        },
+        sections = {
+          lualine_x = {
+            status.listen
+          }
+        }
+      }
+    end
   }
 
   -- Check if packer has been recently installed, sync changes
