cca8397831a3d395656445ff1a81328d216b3299
Ciaron Howell <ciaron.howell@stfc.ac.uk>
2024-04-24 00:02:40 +0100

    Updating ls + ll to use eza

--- .config/fish/functions/ll.fish
+++ .config/fish/functions/ll.fish
@@ -1,3 +1,3 @@
-function ll --wraps=ls --wraps='exa --long --git --icons --classify --header --no-permissions' --description 'alias ll exa --long --git --icons --classify --header --no-permissions'
-  exa --long --git --icons --classify --header --no-permissions $argv; 
+function ll --wraps=ls --wraps='eza --long --git --icons --classify --header --no-permissions' --description 'alias ll exa --long --git --icons --classify --header --no-permissions'
+  ls --long --git --icons --classify --header --no-permissions $argv; 
 end
