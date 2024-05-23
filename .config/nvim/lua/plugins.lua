8fa66887044d9a3a66300ad7f9b0b9ea6aac6ab2
Ciaron Howell <ciaron.howell@stfc.ac.uk>
2024-05-23 11:06:31 +0100

    Making term slightly transparent

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -4,6 +4,7 @@ return {
 		"catppuccin/nvim",
 		name = "catppuccin",
 		config = function()
+			require("catppuccin").setup({ transparent_background = true })
 			vim.cmd([[colorscheme catppuccin]])
 		end,
 	},
